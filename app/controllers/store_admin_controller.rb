f151ad268ada9b7e331d304f89e072a88f41934a
ken <kennethmanuel250@gmail.com>
2020-10-22 22:37:53 -0400

    Done.

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,5 +1,13 @@
 class StoreAdminController < ApplicationController
-    layout "admin"
+    layout 'admin'
     def home
-    end 
-end
+    end
+  
+    def orders
+      render :layout => "order_administration"
+    end
+  
+    def invoice
+      render :layout => false
+    end
+  end
